21ae01c9114f3a7e9e7439465f1c2dc4ca570c64
Matthew McCullough <matthewm@ambientideas.com>
2009-02-17 22:26:46 -0700

    Changed iPhone web service calls to be asynchronous

--- iPhoneConsumer-REST/Classes/IPhoneWSConsumerViewController.m
+++ iPhoneConsumer-REST/Classes/IPhoneWSConsumerViewController.m
@@ -2,7 +2,6 @@
 //  Free for any use for any purpose.  No license restrictions.
 
 #import "IPhoneWSConsumerViewController.h"
-#import "AIWSObject.h"
 
 @implementation IPhoneWSConsumerViewController
 
@@ -10,15 +9,11 @@
 @synthesize pickerData;
 
 
-//- (void)loadView {
-//	[super loadView];
-//	[activityIndicator stopAnimating];
-//}
-
-
-- (IBAction) addContestant:(id) sender {	
-	[self textFieldShouldReturn:txtContestantName];
+- (IBAction) addContestant:(id) sender {
+	NSLog (@"addContestant");
+	[txtContestantName resignFirstResponder];
 	
+	NSLog (@"startAnimating");
 	[activityIndicator startAnimating];
 	
 	NSString* statusText;
@@ -34,23 +29,21 @@
 	lblStatus.text = statusText;
 	
 	//Web Service call
-	AIWSObject *webservice = [[AIWSObject alloc] init];
-	[webservice initiateRESTAddName:txtContestantName.text];
+	NSLog (@"webService AddName");
+	NSLog(statusText);
+	[self initiateRESTAddName:txtContestantName.text];
 	
-	[activityIndicator stopAnimating];
-	
-	[webservice release];
 	[statusText release];
 }
 
 
 - (IBAction) pickWinner:(id) sender {
+	NSLog (@"pickWinner");
 	NSString* statusText;
 	NSString* winnerName = @"DEFAULT";
 
 	//Call WS
-	AIWSObject *webservice = [[AIWSObject alloc] init];
-	winnerName = [webservice initiateRESTPickWinner];
+	winnerName = [self initiateRESTPickWinner];
 
 	statusText = [@"Winner is: " stringByAppendingString: winnerName];
 	
@@ -67,7 +60,6 @@
 	}
 	
 	[winnerName release];
-	[webservice release];
 }
 
 	
@@ -77,6 +69,7 @@
  */
 - (BOOL)textFieldShouldReturn:(UITextField *)theTextField 
 {
+	NSLog (@"textFieldShouldReturn");
 	[theTextField resignFirstResponder];
 	return YES;
 }
@@ -87,6 +80,7 @@
  */
 - (void)viewDidLoad 
 {
+	NSLog (@"viewDidLoad");
 	//NSMutableArray  *array  = [[NSMutableArray alloc] initWithObjects: @"Luke", @"Leia", @"Han", nil];
 	NSMutableArray  *array  = [[NSMutableArray alloc] initWithObjects: nil];
 	self.pickerData  = array;
@@ -119,6 +113,147 @@
 			forComponent: (NSInteger)component
 {
 	return [pickerData objectAtIndex: row];
+}
+
+//WEB SERVICE
+//////////////////////////////////////////
+
+/**
+ * Add a name to the contestant list via a web service call.
+ * Appears to leak memory on the sendSynchronousRequest call.
+ * Apple acknowledges. Hasn't fixed it since October 2008 at a minimum on the 2.2 firmware/SDK
+ * http://lists.apple.com/archives/Macnetworkprog/2008/Nov/msg00013.html
+ * and
+ * http://discussions.apple.com/thread.jspa?messageID=8200590
+ *
+ */
+- (void)initiateRESTAddName:(NSString*) contestantName
+{
+	[activityIndicator startAnimating];
+	
+	NSString *baseURLString = @"http://Opus.local:9090/drawing/";
+	NSString *urlString = [[NSString alloc] initWithFormat:@"%@%@", baseURLString, contestantName];
+	NSURL *url = [[NSURL alloc] initWithString:urlString];
+	NSMutableURLRequest *req = [[NSMutableURLRequest alloc] initWithURL:url];
+	[req setHTTPMethod:@"PUT"];
+	
+	//Asynchronous
+	NSURLConnection *connection = [[NSURLConnection alloc]
+								   initWithRequest:req
+								   delegate:self];
+	
+	[urlString release];
+	[url release];
+	[req release];
+	[connection release];
+}
+
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	NSLog (@"connectionDidReceiveResponse");
+	
+	NSLog(@"Response Code: %d", [response statusCode]);
+	NSLog(@"Content-Type: %@", [[response allHeaderFields] objectForKey:@"Content-Type"]);
+	//Good response codes are 200 through 299
+	if ([response statusCode] >= 200 && [response statusCode] < 300 && [response statusCode] != 204) {
+		//Call was successful
+		NSLog(@"Web service call deemed successful based on status code.");
+	}
+	else {
+		//Bad response codes are 204 (null payload) and 400 series
+		UIAlertView *errorAlert = [[UIAlertView alloc]
+								   initWithTitle: @"Error"
+								   message: [[NSString alloc] initWithFormat:@"Error in Web Service call. Response code %d", [response statusCode]]
+								   delegate:nil
+								   cancelButtonTitle:@"OK"
+								   otherButtonTitles:nil];
+		[errorAlert show];
+		[errorAlert release];
+	}
+}
+
+- (void)connection:(NSURLConnection *)connection
+	didReceiveData:(NSData *)data {
+	NSLog (@"connectionDidReceiveData");
+	NSString *newText = [[NSString alloc]
+						 initWithData:data
+						 encoding:NSUTF8StringEncoding];
+	if (newText != NULL) {
+		NSLog(newText);
+		//[self appendTextToView:newText];
+		[newText release];
+	}
+}
+
+- (void) connectionDidFinishLoading: (NSURLConnection*) connection {
+	NSLog (@"connectionDidFinishLoading");
+	[activityIndicator stopAnimating];
+}
+
+-(void) connection:(NSURLConnection *)connection
+  didFailWithError: (NSError *)error {
+	UIAlertView *errorAlert = [[UIAlertView alloc]
+							   initWithTitle: [error localizedDescription]
+							   message: [error localizedFailureReason]
+							   delegate:nil
+							   cancelButtonTitle:@"OK"
+							   otherButtonTitles:nil];
+	[errorAlert show];
+	[errorAlert release];
+	[activityIndicator stopAnimating];
+	NSLog (@"Connection Failed with Error");
+}
+
+
+
+
+
+/**
+ * Pick a winner from the contestant list via a web service call and return their name.
+ */
+- (NSString*)initiateRESTPickWinner
+{
+	NSString *baseURLString = @"http://Opus.local:9090/drawing/";
+	NSURL *url = [[NSURL alloc] initWithString:baseURLString];
+	NSLog(@"Pick Winner URL: %d", *url);
+	NSMutableURLRequest *req = [[NSMutableURLRequest alloc] initWithURL:url];
+	[req setHTTPMethod:@"GET"];
+	NSLog(@"Pick Winner request: %d", *req);
+	
+	BOOL success = false;
+	int retryCount = 0;
+	NSString *result = nil;
+	
+	// Retry the web service call up to three times
+	while (success == false && retryCount <3) {
+		NSHTTPURLResponse* response = nil;  
+		NSError* error = nil;  
+		NSData *responseData = [NSURLConnection sendSynchronousRequest:req   
+													 returningResponse:&response  
+																 error:&error];  
+		result = [[NSString alloc] initWithData:responseData 
+									   encoding:NSUTF8StringEncoding];
+		NSLog(@"Response Code: %d", [response statusCode]);
+		NSLog(@"Content-Type: %@", [[response allHeaderFields] 
+									objectForKey:@"Content-Type"]);
+		//Log the usual HTTP 200 through 299 responses
+		if ([response statusCode] >= 200 && [response statusCode] < 300) {
+			NSLog(@"Result: %@", result);
+			success = true;
+		}
+		//HTTP 204 is a NULL Payload which we occasionally and inexplicably get. Highly reproducible.
+		if ([response statusCode] == 204) {
+			[result release];
+			success = false;
+		}
+		
+		retryCount++;
+	}
+	
+	[url release];
+	[req release];
+	
+	return result;
 }
 
 @end
